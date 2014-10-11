d478a7ad56e9620aba2705c849a8516873344ae8
Kunal <kunaldhir@live.in>
2014-10-11 21:15:14 +0530

    Update members.php

--- members.php
+++ members.php
@@ -19,11 +19,10 @@ include 'connection.php';
 
 global $id;
 $username = $_SESSION['username'];
-$result=mysql_query("SELECT * FROM data WHERE id='$id'") or die('Invalid query: ' . mysql_error());
-	$data=mysql_fetch_array($result);
+$users=mysql_query("SELECT * FROM data WHERE id='$id'") or die('Invalid query: ' . mysql_error());
+	$userdata=mysql_fetch_array($users);
 	
 	echo "<center>"."<h2>"."Welcome ".$_SESSION['username']."</h2>"."</center>"; 
-	
 ?>
 
 <center>
@@ -33,9 +32,17 @@ $result=mysql_query("SELECT * FROM data WHERE id='$id'") or die('Invalid query: 
 <a href="logout.php">Log Out</a>
 </nav>
 <center>
+<h3>Your projects</h3>
 
 <?php
+
+$projects=mysql_query("SELECT * FROM projects WHERE username='$username'") or die('Invalid query:'.mysql_error());
 	
+	while($projectdata=mysql_fetch_array($projects);){
+	
+	echo "<ul><li>Project name :".$projectdata['name']."</li>";
+	echo "<li>Contributions :"$projectdata['contributions']."</li></ul>";}
+
 $expire = time();
 
 ?>
