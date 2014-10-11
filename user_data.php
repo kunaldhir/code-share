a5044c2896097af1bb13233eed47ece18a86a618
Kunal <kunaldhir@live.in>
2014-10-11 22:21:59 +0530

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -11,20 +11,7 @@ $data=mysql_fetch_array($result);
 
 if (isset($_POST["edit_user"])){
 	
-	echo"<form method='POST' action='eval($_SERVER['PHP_SELF'];)'>";
-	echo"New username : <input type='text' name='new_username'><br><br>";
-	echo"Password : <input type='text' name='password'><br><br>";
-	echo"<input type='submit' value='username_submit' name='username_submit'>";
-	
-	if (isset($_POST["username_submit")){
-		
-		if ($data['password']==$_POST["password"]){
-		
-			$username=$_POST['username'];
-			$result = mysql_query("UPDATE * SET username='$username' WHERE id='$id' ") or die('Invalid query: ' . mysql_error());
-			echo "Data updated successfully !";
-		}
-	}
+	header(location:'edit_username.php');
 	
 }
 ?>
