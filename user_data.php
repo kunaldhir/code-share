fc7de1d7207f813e4b05ed43fb208acd60f27aba
Kunal <kunaldhir@live.in>
2014-10-11 22:18:36 +0530

    Removed bugs

--- user_data.php
+++ user_data.php
@@ -5,9 +5,9 @@ echo "<center><h2>Your information</h2></center>";
 $result=mysql_query("SELECT * FROM data WHERE id='$id'") or die ('Invalid query :'.mysql_error());
 $data=mysql_fetch_array($result);
 	
-	echo "Username :".$data['username']."<button id="edit_user">Edit username</button><br>";
-	echo "Email ID :".$data['email']."<button>Edit email id</button><br>";
-	echo "Password :".$data['password']."<button>Edit password</button><br>";
+	echo "Username :".$data['username']."<button id='edit_user'>Edit username</button><br>";
+	echo "Email ID :".$data['email']."<button id='edit_email'>Edit email id</button><br>";
+	echo "Password :".$data['password']."<button id='edit_password'>Edit password</button><br>";
 
 if (isset($_POST["edit_user"])){
 	
